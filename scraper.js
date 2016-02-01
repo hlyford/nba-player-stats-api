592c67d1c5d6102970dbe974199cdd24d53fe5e1
henry <hlyford11@gmail.com>
2016-01-31 18:47:03 -0800

    Modify script to handle mistakenly selected elements

--- scraper.js
+++ scraper.js
@@ -23,18 +23,21 @@ var getPlayers = function(urlSlug, callback) {
   			// for each player, make a second request using cheerio
   			// the new page is the href for the image
   			request(playerUrl, function (error, response, html) {
+  				if (error) throw error;
 	  			if (!error && response.statusCode == 200) {			  	
 	    			var $$ = cheerio.load(html);    
 	    			var playerPicUrl = $$('.main-headshot').find('img').attr('src');			    
-	    			playerList.push({name: playerName, imageUrl: playerPicUrl});			  			    				    
+	    			if (playerPicUrl !== undefined) {
+		    			playerList.push({name: playerName, imageUrl: playerPicUrl});			  			    				    
 
-	    			// get the images and save them to the images directory				    
-	    			var dlDir = './images/' + shortSlug + '/'  + playerName.replace(/ /g,"_") + '.png';				    		           	
-      			var curl =  'curl ' + playerPicUrl.replace(/&/g,'\\&') + ' -o ' + dlDir  + ' --create-dirs';
-      			var child = exec(curl, function(err, stdout, stderr) {
-          		if (err){ console.log(stderr); throw err; } 
-          		else console.log(playerName + ' downloaded to ' + dlDir);
-      			});
+		    			// get the images and save them to the images directory				    
+		    			var dlDir = './images/' + shortSlug + '/'  + playerName.replace(/ /g,"_") + '.png';				    		           	
+	      			var curl =  'curl ' + playerPicUrl.replace(/&/g,'\\&') + ' -o ' + dlDir  + ' --create-dirs';
+	      			var child = exec(curl, function(err, stdout, stderr) {
+	          		if (err){ console.log(stderr); throw err; } 
+	          		else console.log(playerName + ' downloaded to ' + dlDir);
+	      			});
+	      		}
 	  			}
 				});
 			});    	    
