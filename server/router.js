018714f6238e855bbb42edf958af5cd244d79b09
Henry Lyford <hlyford@commonsense.org>
2018-01-20 15:19:41 -0800

    fix merge issue

--- server/router.js
+++ server/router.js
@@ -16,29 +16,58 @@ router.get('/', function(req, res){
 router.get('/players/:lastname/:firstname?', function(req, res){
 
 	var firstname = (req.params.firstname) ? req.params.firstname.toLowerCase() : false;
-	var lastname = req.params.lastname.toLowerCase();	
-	
-	// if both names are provided	
-	if (firstname) {				
-		var fullName = firstname + "_" + lastname + ".png";	
+	var lastname = req.params.lastname.toLowerCase();
+
+	// if both names are provided
+	if (firstname) {
+		var fullName = firstname + "_" + lastname + ".png";
 		console.log("Looking for : ", fullName);
-			
+
 		// search the team directories until finds right name
-		Finder.from('./images').findFiles(fullName, function(files) {	
+		Finder.from('./images').findFiles(fullName, function(files) {
 			if (files.length === 0) res.send( 'Sorry, that player was not found. Please check the spelling.');
 			else res.sendFile(files[0]);
-		});	
+		});
 
 	// if only the last name is supplied
-	} else {		
+	} else {
 		console.log("Looking for : ", lastname);
-		Finder.from('./images').findFiles('*' + lastname + '*', function(files) {	
+		Finder.from('./images').findFiles('*' + lastname + '*', function(files) {
 			if (files.length === 0) res.send( 'Sorry, that player was not found. Please check the spelling.');
 			else res.sendFile(files[0]);
-		});	
+		});
 	}
-	 
+
 });
+
+// Get player data
+router.get('/players-stats/:lastname/:firstname?', function(req, res){
+
+	var firstname = (req.params.firstname) ? req.params.firstname.toLowerCase() : false;
+	var lastname = req.params.lastname.toLowerCase();
+
+	// if both names are provided
+	if (firstname) {
+		var fullName = firstname + "_" + lastname + ".json";
+		console.log("Looking for : ", fullName);
+
+		// search the team directories until finds right name
+		Finder.from('./player_data').findFiles(fullName, function(files) {
+			if (files.length === 0) res.send( 'Sorry, that player was not found. Please check the spelling.');
+			else res.sendFile(files[0]);
+		});
+
+	// if only the last name is supplied
+	} else {
+		console.log("Looking for last name: ", lastname);
+		Finder.from('./player_data').findFiles('*' + lastname + '*', function(files) {
+			if (files.length === 0) res.send( 'Sorry, that player was not found. Please check the spelling.');
+			else res.sendFile(files[0]);
+		});
+	}
+
+});
+
 
 router.get('/*', function(req, res) {
 	res.redirect('/');
